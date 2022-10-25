57e28f6b79519072a1c6817dcbc923737174399f
Y-Saruwatari <e1b20049@st.oit.ac.jp>
2022-10-25 15:00:00 +0900

    step3の実装

--- src/main/java/oit/is/sannomiya/lec05/lec05/security/Sample3AuthConfiguration.java
+++ src/main/java/oit/is/sannomiya/lec05/lec05/security/Sample3AuthConfiguration.java
@@ -64,7 +64,7 @@ public class Sample3AuthConfiguration {
     // mvcMatchers().authenticated()がmvcMatchersに指定されたアクセス先に認証処理が必要であることを示す
     // authenticated()の代わりにpermitAll()と書くと認証不要となる
     http.authorizeHttpRequests()
-        .mvcMatchers("/").authenticated();
+        .mvcMatchers("/sample5/**").authenticated();
     http.logout().logoutSuccessUrl("/"); // ログアウト時は "http://localhost:8000/" に戻る
     /**
      * 以下2行はh2-consoleを利用するための設定なので，開発が完了したらコメントアウトすることが望ましい
